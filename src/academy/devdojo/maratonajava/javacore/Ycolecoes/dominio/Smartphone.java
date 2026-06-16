9e9978e2e7675e3528af594fa0ab64ae7f3f8c9e
victor-Silvadev <notebookconta037@gmail.com>
2026-06-16 20:16:40 -0300

    Lesson about Collections(equals, hashCode), Maratona Java - DevDojo.

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -0,0 +1,53 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.dominio;
+
+public class Smartphone {
+    private String serialNumber;
+    private String marca;
+
+
+    public Smartphone(String serialNumber, String marca) {
+        this.serialNumber = serialNumber;
+        this.marca = marca;
+    }
+
+    // Reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
+    // Simetrico: para x e y diferentes de null, se x.equals(y) == true logo, y.equals(x) == true
+    // Transitividade: para x,y,z diferentes de null, se x.equals(y) == true, e x.equals(z) == true logo, y.equals(z) == true
+    // Consistente: x.equals(x) sempre retorna true se x for diferente de null
+    // para x diferente de null, x.equals(null) tem que retornar false.
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (this.getClass() != obj.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+        return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+    }
+
+    // se x.equals(y) == true, y.hashcode() == x.hashcode()
+    // y.hashcode() == x.hashcode() nap necessariamente o equals de y.equals(x) tem que ser true
+    // x.equals(y) == false
+    // y.hashcode() != x.hashcode() x.equals(y) devera ser false.
+
+    @Override
+    public int hashCode() {
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTeste01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
+
+public class EqualsTeste01 {
+    public static void main(String[] args) {
+
+        Smartphone s1 = new Smartphone("1ABC1", "Iphone");
+        Smartphone s2 = new Smartphone("1ABC1", "Iphone");
+
+        System.out.println(s1.equals(s2));
+    }
+}
